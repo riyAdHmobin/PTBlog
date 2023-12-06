fbd1e37965d8a524da8b0e8b8ad3e4fe9c69b399
riyadhmobin <riyadhmobin@gmail.com>
2023-12-06 19:29:13 +0600

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class,'index']);
+});
+
